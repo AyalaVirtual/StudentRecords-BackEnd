5c0ffba0dcdd6e6e790f883cce1de8e03b738971
AyalaVirtual <erica.ayala.work@gmail.com>
2024-04-21 22:20:53 -0400

    Add GET by id method to service class.

--- src/main/java/org/example/service/StudentService.java
+++ src/main/java/org/example/service/StudentService.java
@@ -7,6 +7,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
+import java.util.Optional;
 
 
 @Service
@@ -33,6 +34,23 @@ public class StudentService {
             throw new InformationNotFoundException("student list not found");
         } else {
             return studentList;
+        }
+    }
+
+
+    /**
+     * This is a GET request that checks to see if an individual student exists before either returning it, or throwing an InformationNotFoundException.
+     *
+     * @param studentId represents the id of the specific student the user is trying to get
+     * @return student by id if they exist
+     */
+    public Optional<Student> getStudentById(Long studentId) {
+        Optional<Student> studentOptional = studentRepository.findById(studentId);
+
+        if (studentOptional.isPresent()) {
+            return studentOptional;
+        } else {
+            throw new InformationNotFoundException("student with student id " + studentId + " not found");
         }
     }
 
