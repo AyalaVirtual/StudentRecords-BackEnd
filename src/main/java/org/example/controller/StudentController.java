8cd6d45987775502b171ded34361579a1dd6b769
AyalaVirtual <erica.ayala.work@gmail.com>
2024-04-21 23:09:01 -0400

    Add GET all method to controller class with doc strings.

--- src/main/java/org/example/controller/StudentController.java
+++ src/main/java/org/example/controller/StudentController.java
@@ -1,9 +1,49 @@
 package org.example.controller;
 
-import org.springframework.stereotype.Controller;
+import org.example.model.Student;
+import org.example.service.StudentService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
-@Controller
+import java.util.List;
+
+
+@RestController
+@RequestMapping("/api/") // http://localhost:9092/api/
 public class StudentController {
+    private StudentService studentService;
+
+
+    @Autowired
+    public void setStudentService(StudentService studentService) {
+        this.studentService = studentService;
+    }
+
+
+    /**
+     * This sets the path for GET requests for all students and checks if the list of students is empty or not before deciding whether to send an HTTP status message of NOT FOUND or OK
+     *
+     * @return the HTTP status message
+     */
+    @GetMapping(path = "/students/")
+    public ResponseEntity<?> getAllStudents() {
+        List<Student> studentList = studentService.getAllStudents();
+
+        if (studentList.isEmpty()) {
+            message.put("message", "cannot find any students");
+            return new ResponseEntity<>(message, HttpStatus.NOT_FOUND);
+        } else {
+            message.put("message", "success");
+            message.put("data", studentList);
+            return new ResponseEntity<>(message, HttpStatus.OK);
+        }
+    }
+
+
 
 
 }
--- src/main/java/org/example/service/StudentService.java
+++ src/main/java/org/example/service/StudentService.java
@@ -6,14 +6,12 @@ import org.example.model.Student;
 import org.example.repository.StudentRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
-
 import java.util.List;
 import java.util.Optional;
 
 
 @Service
 public class StudentService {
-
     private StudentRepository studentRepository;
 
 
