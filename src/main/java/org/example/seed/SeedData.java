7b8d0a412c883cf8897b32d9c9e70ca92368ecea
AyalaVirtual <erica.ayala.work@gmail.com>
2024-04-22 10:52:15 -0400

    Edit seed data to reflect change to model's attributes.

--- src/main/java/org/example/seed/SeedData.java
+++ src/main/java/org/example/seed/SeedData.java
@@ -21,33 +21,33 @@ public class SeedData implements CommandLineRunner {
     @Override
     public void run(String... args) throws Exception {
         Student student1 = new Student();
-        student1.setFirstName("John");
-        student1.setLastName("Smith");
-        student1.setGrade(92);
+        student1.setName("Jon Snow");
+        student1.setEmail("princeWhoWasPromised@winterfell.com");
+        student1.setDateOfBirth("08/14/1988");
         studentRepository.save(student1);
 
         Student student2 = new Student();
-        student2.setFirstName("");
-        student2.setLastName("");
-        student2.setGrade(87);
+        student2.setName("Daenerys Targaryen");
+        student2.setEmail("motherOfDragons@dragonstone.com");
+        student2.setDateOfBirth("06/30/1994");
         studentRepository.save(student2);
 
         Student student3 = new Student();
-        student3.setFirstName("");
-        student3.setLastName("");
-        student3.setGrade(82);
+        student3.setName("Jaime Lannister");
+        student3.setEmail("kingslayer@casterlyrock.com");
+        student3.setDateOfBirth("07/02/1977");
         studentRepository.save(student3);
 
         Student student4 = new Student();
-        student4.setFirstName("");
-        student4.setLastName("");
-        student4.setGrade(70);
+        student4.setName("Arya Stark");
+        student4.setEmail("noOne@facelessMen.com");
+        student4.setDateOfBirth("03/01/2008");
         studentRepository.save(student4);
 
         Student student5 = new Student();
-        student5.setFirstName("");
-        student5.setLastName("");
-        student5.setGrade(75);
+        student5.setName("Olenna Tyrell");
+        student5.setEmail("queenOfThorns@highgarden.com");
+        student5.setDateOfBirth("05/12/1972");
         studentRepository.save(student5);
 
     }
