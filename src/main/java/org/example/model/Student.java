6280a81e4685fe405ab798840511a942e5231006
AyalaVirtual <erica.ayala.work@gmail.com>
2024-04-21 21:21:08 -0400

    Finish Student model class.

--- src/main/java/org/example/model/Student.java
+++ src/main/java/org/example/model/Student.java
@@ -1,7 +1,77 @@
 package org.example.model;
 
-import javax.persistence.Entity;
+import javax.persistence.*;
+
 
 @Entity
+@Table(name="students")
 public class Student {
+    @Id
+    @Column
+    @GeneratedValue(strategy= GenerationType.IDENTITY)
+    private Long id;
+
+    @Column
+    private String firstName;
+
+    @Column
+    private String lastName;
+
+    @Column
+    private int grade;
+
+
+    public Student() {
+    }
+
+    public Student(Long id, String firstName, String lastName, int grade) {
+        this.id = id;
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.grade = grade;
+    }
+
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public int getGrade() {
+        return grade;
+    }
+
+    public void setGrade(int grade) {
+        this.grade = grade;
+    }
+
+
+    @Override
+    public String toString() {
+        return "Student{" +
+                "id=" + id +
+                ", firstName='" + firstName + '\'' +
+                ", lastName='" + lastName + '\'' +
+                ", grade=" + grade +
+                '}';
+    }
 }
