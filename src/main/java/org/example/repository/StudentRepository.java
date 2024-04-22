c0ff28784065f0210e37746b08e70b0ad891a5f2
AyalaVirtual <erica.ayala.work@gmail.com>
2024-04-22 10:54:54 -0400

    Update method signatures in repository class to reflect changes to model's attributes.

--- src/main/java/org/example/repository/StudentRepository.java
+++ src/main/java/org/example/repository/StudentRepository.java
@@ -8,12 +8,12 @@ import java.util.Optional;
 
 @Repository
 public interface StudentRepository extends JpaRepository<Student, Long> {
-    // This method finds a fizzbuzz by  its id
+    // This method finds a student by  its id
     Optional<Student> findById(Long studentId);
 
-    // This method finds a student by their full name
-    Student findByFullName(String studentFirstName, String studentLastName);
+    // This method finds a student by their name
+    Student findByName(String studentName);
 
-    // This method finds a student by their last name
-    Student findByLastName(String studentLastName);
+    // This method finds a student by their email address
+    Student findByEmail(String studentEmail);
 }
